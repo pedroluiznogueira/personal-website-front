d5ca69147d2806e62881b6637a58f571cc7407fe
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-16 23:50:04 -0300

    Implemented first and second section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-website-front';
+
+  goToLink(url: string){
+    window.open(url, "_blank");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faSquare, faCheckSquare, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { faStackOverflow, faJava, faAngular,faGithub, faMedium, faLinkedin, faReact } from '@fortawesome/free-brands-svg-icons';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library
+      .addIcons(
+        faSquare, 
+        faCheckSquare, 
+        faEnvelope, 
+        farSquare, 
+        farCheckSquare, 
+        faStackOverflow, 
+        faGithub, 
+        faMedium, 
+        faLinkedin, 
+        faReact,
+        faAngular,
+        faJava
+      );
+  }
+}
