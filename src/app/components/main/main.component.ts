9fd7bd040b679a73b1030998963ee5d287b04dc7
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-02 18:51:04 -0300

    Setted up all email related

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,13 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  name?: string;
+  info?: string;
+  email?: string;
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
 
   goToLink(url: string){
     window.open(url, "_blank");
+  }
+
+  onSubmit() {
+    let obj: Object = {
+      name: this.name,
+      info: this.info,
+      email: this.email
+    };
+
+    this.http.post("http://localhost:8080/contact/me", obj)
+      .subscribe(
+        (res) => {
+          this.name = '';
+          this.email = '';
+          this.info = '';
+        }
+      );
+
+    console.log(obj)
   }
 
 }
